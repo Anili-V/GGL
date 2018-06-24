9fa3e770015bc85b58c72b0004d336943d9d13c5
Anili-V <40320684+Anili-V@users.noreply.github.com>
2018-06-24 17:14:41 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+$msg = '';
+$msgClass = '';
 
 session_start();
 
@@ -12,22 +14,22 @@ if (isset($_POST['submit'])) {
 	//Error Handlers
 	//Check if inputs are inputy
 	if (empty($uid) OR empty($pwd)){
-		header("Location: index.php?login=error");
-		exit();
+		$msg = 'Please fill in all fields';
+		$msgClass = 'alert-danger';
 	} else {
 		$sql = "SELECT * FROM users WHERE user_uid='$uid' OR user_email='$uid'";
 		$result = mysqli_query($conn, $sql);
 		$resultCheck = mysqli_num_rows($result);
 		if ($resultCheck < 1) {
-			header("Location: index.php?login=error");
-			exit();
+			$msg = 'Incorrect Username';
+			$msgClass = 'alert-danger';
 		} else {
 			if ($row = mysqli_fetch_assoc($result)){
 				//De-hashing the password
 				$hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
 				if ($hashedPwdCheck === false) {
-					header("Location: index.php?login=error");
-				exit();
+					$msg = 'Incorrect Password';
+					$msgClass = 'alert-danger';
 				} elseif ($hashedPwdCheck === true) {
 					//Login the user here
 					$_SESSION['u_id'] = $row['user_id'];
@@ -35,13 +37,38 @@ if (isset($_POST['submit'])) {
 					$_SESSION['u_last'] = $row['user_last'];
 					$_SESSION['u_email'] = $row['user_email'];
 					$_SESSION['u_uid'] = $row['user_uid'];
-					header("Location: index.php?login=success");
-					exit();
+					$msg = 'Logged In';
+					$msgClass = 'alert-success';
 				}
 			}
 		}
 	}
-} else {
-	header("Location: index.php?login=error");
-	exit();
 }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+    <div>	
+    	<?php if($msg != ''): ?>
+    		<div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+    	<?php endif; ?>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	      <div>
+		      <label>Username/Email</label>
+		      <input type="text" name="uid" value="<?php echo isset($_POST['uid']) ? $uid : ''; ?>">
+	      </div>
+	      <div>
+	      	<label>Email</label>
+	      	<input type="text" name="pwd" value="<?php echo isset($_POST['pwd']) ? $pwd : ''; ?>"> <!-- In the code if an error occurs for one field than all the fields inputted data is deleted. So the value attribute checks whether some data has been inputted and if yes than the data is shown if no then it is blank (the input box is blank)-->
+	      </div>
+	      <br>
+	      <button type="submit" name="submit">Submit</button>
+      </form>
+    </div>
+</body>
+</html>
