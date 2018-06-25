cb795a0a916ed77c31f2e9f8e7d05e4a7a6ff518
Anili-V <40320684+Anili-V@users.noreply.github.com>
2018-06-25 10:32:10 +0530

    Update database.php

--- database.php
+++ database.php
@@ -3,6 +3,6 @@
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "r20635Ani";
-$dbName = "loginsystem";
+$dbName = "login";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
