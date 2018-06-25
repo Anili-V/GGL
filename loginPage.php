7da0bb4f150516bd247aad92c2c99c643939e0e3
Anili-V <40320684+Anili-V@users.noreply.github.com>
2018-06-25 09:29:48 +0530

    Delete loginPage.php

--- loginPage.php
+++
@@ -1,5 +0,0 @@
-<form action='login.php' method="POST">
-	<input type="text" name="uid" placeholder="Username/e-mail">
-	<input type="password" name="pwd" placeholder="Password">
-	<button type="submit" name="submit">Login</button>
-</form>
