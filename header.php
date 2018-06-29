bb2eaf02f2aa5a9b7b39ddcf3e81e691c72d5c56
Anili-V <40320684+Anili-V@users.noreply.github.com>
2018-06-29 14:28:03 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+<style> 
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {background-color: #111;
+    color: #ffa600;}
+
+
+#jo{width: 1340px;}
+#gj{font-family:comic sans MS;padding-left: 550px;}
+#go{font-family:comic sans MS;padding-left: 590px;}
+#gh{font-family:comic sans MS;padding-left: 470px;}
+#gi{font-family:comic sans MS;}
+
+</style>
+
+</head>
+<body>
+<img src="C:\Users\sanat\Desktop\ggl.png" id="jo">
+<ul>
+  <li><a class="active" href="#home" >HOME</a></li>
+  <li><a href="#news">GAMES</a></li>
+  <li><a href="#contact">REGISTRATION</a></li>
+  <li><a href="#about">RULES</a></li>
+  <li><a href="#about">CONTACT </a></li>
+</ul><br>
+<br>
+<br>
