8e46312b136a45a9f496c71ddc21cbf56e60c1ce
Anili-V <40320684+Anili-V@users.noreply.github.com>
2018-06-29 14:30:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="shortcut icon" type="image/png" href="favicon.png">
-<style> 
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-}
-
-li {
-    float: left;
-}
-
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-
-li a:hover {background-color: #111;
-    color: #ffa600;}
-
-
-#jo{width: 1340px;}
-#gj{font-family:comic sans MS;padding-left: 550px;}
-#go{font-family:comic sans MS;padding-left: 590px;}
-#gh{font-family:comic sans MS;padding-left: 470px;}
-#gi{font-family:comic sans MS;}
-
-</style>
-
-</head>
-<body>
-<img src="C:\Users\sanat\Desktop\ggl.png" id="jo">
-<ul>
-  <li><a class="active" href="#home" >HOME</a></li>
-  <li><a href="#news">GAMES</a></li>
-  <li><a href="#contact">REGISTRATION</a></li>
-  <li><a href="#about">RULES</a></li>
-  <li><a href="#about">CONTACT </a></li>
-</ul><br>
-<br>
-<br>
+<?php include 'header.php'; ?>
 <H1 id="gj">ABOUT US</H1><br>
 <h1 id="gh"><B>How this all started ?</B></h1>
 <h2 id="gi">On getting inspiration from Narendra Modi Ji's "Mann Ki Baat" serial on TV GGL was formed with the vision of making a website to make Indian youngsters fit. There was one lady, an Indian citizen who shared her thoughts with Modi Ji in the serial. She asked him that what will he do to take away phones from youngsters hands and encourage them to play. In today's time this is a big question for all parents, teachers and youngsters too. Sports is a good alternative to mobiles and other screens.</h2><br><br><br>
 <H1 id="go">Aim</H1>
 <h2 id="gi">Golden Games League(GGL) aims to create a common structure for urban and rural India for competition across sports. It also aims to promote participation from all Indian youngsters irrespective of economic & financial status, sex, literacy, location etc. The games which will be played on this platform are not regular games like cricket, football, basketball, tennis but are gully games like pithoo, gallery, dog-in-the-bone etc. These are long forgotton old games. Old games are Golden games. Hence the name, Golden Games League.   </h2>
-
-</body>
-
-</html>
+<?php include 'footer.php'; ?>
